850bb62d955f7141856a2be7486abc5c4d1781b4
shuhelahm <shuhel037@gmail.com>
2022-03-02 13:17:06 +0600

    post and get data by express(first commit)

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+app.use(cors());
+app.use(express.json());
+
+// user: tourist-corner\
+// pass: YuODHZn19Gcm4YH9
+
+
+
+const uri = "mongodb+srv://tourist-corner:YuODHZn19Gcm4YH9@cluster0.oesrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("tourist-corner");
+    const offersCollection = database.collection("offers");
+
+    // get api
+    app.get('/offers',async(req,res)=>{
+      const cursor = offersCollection.find({});
+      const offers = await cursor.toArray();
+      res.send(offers)
+    })
+
+    // post api
+    app.post('/offers', async(req,res)=>{
+      const newOffer = req.body;
+      const result = await offersCollection.insertOne(newOffer)
+      console.log('got new user', req.body)
+      console.log('added user', result)
+      res.json(result)
+    })
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
