aa47ac56c6e5aefaa9a83338ae212be93acc7591
shuhelahm <shuhel037@gmail.com>
2022-03-03 08:24:37 +0600

    get api created for id of offers

--- index.js
+++ index.js
@@ -3,13 +3,9 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
-
-// user: tourist-corner\
-// pass: YuODHZn19Gcm4YH9
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oesrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -27,6 +23,15 @@ async function run() {
       res.send(offers)
     })
 
+    // get api
+    app.get('/offers/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)}
+      const offer = await offersCollection.findOne(query)
+      console.log('load user with id',id);
+      res.send(offer)
+    })
+
     // post api
     app.post('/offers', async(req,res)=>{
       const newOffer = req.body;
@@ -41,12 +46,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
-
 
 app.listen(port, () => {
   console.log(`Example  listening${port}`)
