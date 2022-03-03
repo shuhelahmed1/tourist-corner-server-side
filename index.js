7a4a2230e1decb1a30e2f8ec2eb38c7ab6744e9f
shuhelahm <shuhel037@gmail.com>
2022-03-03 18:52:00 +0600

    process.env.port added in port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
