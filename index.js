e51f640a234b055b7f8672deb0357a365c006ef2
shuhelahm <shuhel037@gmail.com>
2022-03-04 16:04:07 +0600

    delete api mistaken problem solved

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ async function run() {
     })
 
     // delete api for orders
-    app.delete('./orders/:id', async(req,res)=>{
+    app.delete('/orders/:id', async(req,res)=>{
       const id = req.params.id;
       const query = {_id: ObjectId(id)};
       const result = await ordersCollection.deleteOne(query)
