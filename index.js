6496042f2b7e37f7577050c53ca5a99f9167b5ea
shuhelahm <shuhel037@gmail.com>
2022-03-04 15:17:32 +0600

    delete api created for orders

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 app.use(cors());
 app.use(express.json());
 
