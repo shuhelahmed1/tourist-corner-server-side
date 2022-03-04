9c6049df79232cbdb1b465a9809f4985dec252ab
shuhelahm <shuhel037@gmail.com>
2022-03-04 15:12:41 +0600

    made My Order page responsive

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ async function run() {
     const offersCollection = database.collection("offers");
     const ordersCollection = database.collection("orders");
 
-    // get api
+    // get api for offers
     app.get('/offers',async(req,res)=>{
       const cursor = offersCollection.find({});
       const offers = await cursor.toArray();
       res.send(offers)
     })
 
-    // get api
+    // get api particular offer id
     app.get('/offers/:id',async(req,res)=>{
       const id = req.params.id;
       const query = {_id: ObjectId(id)}
@@ -33,21 +33,14 @@ async function run() {
       res.send(offer)
     })
 
-    // get api for orders
-    // app.get('/orders',(req,res)=>{
-    //   const cursor = ordersCollection.find({})
-    //   const orders = await cursor.toArray();
-    //   res.send(orders) 
-    // })
-
-    // post api
-    // app.post('/orders', async(req,res)=>{
-    //   const newOrder = req.body;
-    //   const result = await offersCollection.insertOne(newOrder)
-    //   console.log('got new order', req.body)
-    //   console.log('added order', result)
-    //   res.json(result)
-    // })
+    // post api for offers
+    app.post('/offers', async(req,res)=>{
+      const newOffer = req.body;
+      const result = await offersCollection.insertOne(newOffer)
+      console.log('got new user', req.body)
+      console.log('added user', result)
+      res.json(result)
+    })
 
     // get api for orders
     app.get('/orders',async(req,res)=>{
@@ -65,14 +58,14 @@ async function run() {
       res.json(result)
     })
 
-    // post api for offers
-    app.post('/offers', async(req,res)=>{
-      const newOffer = req.body;
-      const result = await offersCollection.insertOne(newOffer)
-      console.log('got new user', req.body)
-      console.log('added user', result)
-      res.json(result)
+    // delete api for orders
+    app.delete('./orders/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await ordersCollection.deleteOne(query)
+      res.json(1)
     })
+   
   } finally {
     // await client.close();
   }
