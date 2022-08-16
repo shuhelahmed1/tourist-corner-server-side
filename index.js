eb6d04d2848a8c5f4ff49c31164efc2a51e59583
shuhelahm <shuhel037@gmail.com>
2022-08-17 01:07:58 +0600

    problem checking

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-app.use(cors());
 app.use(express.json());
 
 // middleware
