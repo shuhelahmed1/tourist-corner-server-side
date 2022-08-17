6735d7afe2c3e214c079dd79d3379579216ce032
shuhelahm <shuhel037@gmail.com>
2022-08-18 00:47:35 +0600

    api created for update order

--- index.js
+++ index.js
@@ -80,14 +80,18 @@ async function run() {
     })
 
     // put api for orders
-    // app.put('/orders/:id', async(req,res)=>{
-    //   const id = req.params.id;
-    //   const query = {_id: ObjectId(id)};
-    //   const options = {upsert: true}
-    //   const updateDoc = {$set: {address: updatedOrder.address}}
-    //   const result = await ordersCollection.updateOne(query,updateDoc,options)
-    //   res.json(result)
-    // })
+    app.put('/orders/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const updatedUser = req.body;
+      const options = {upsert: true}
+      const updateDoc = {$set: {
+        address: updatedUser.address,
+        phone: updatedUser.phone
+      }}
+      const result = await ordersCollection.updateOne(query,updateDoc,options)
+      res.json(result)
+    })
    
   } finally {
     // await client.close();
